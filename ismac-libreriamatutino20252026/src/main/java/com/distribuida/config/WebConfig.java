05a1407d7571a8decb509096f98992bd2e5ad282
1726813684 <mbenitez@tecnologicoismac.edu.ec>
2026-02-02 12:09:19 -0500

    versionamiento 2 en github de front end y back end 2

--- ismac-libreriamatutino20252026/src/main/java/com/distribuida/config/WebConfig.java
+++ ismac-libreriamatutino20252026/src/main/java/com/distribuida/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
 
 
-    // comentarios
+    // comentarios 2
     @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
