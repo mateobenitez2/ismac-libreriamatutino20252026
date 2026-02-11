cad2c7582e1bd4af3f84ac54853df48f31df16f5
1726813684 <mbenitez@tecnologicoismac.edu.ec>
2026-02-11 12:26:28 -0500

    versionamiento 2 en github de front end y back end 2

--- ismac-libreriamatutino20252026/src/main/java/com/distribuida/config/WebConfig.java
+++ ismac-libreriamatutino20252026/src/main/java/com/distribuida/config/WebConfig.java
@@ -3,10 +3,11 @@ package com.distribuida.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
 
     // comentarios 2
@@ -22,4 +23,11 @@ public class WebConfig {
             }
         };
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("*/portadas/**")
+                .addResourceLocations("file:uploads/portadas/*");
+    }
+
 }
